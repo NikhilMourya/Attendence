2c55e4d300591112bee6fb4a2c2921e75eaf38ce
Nikhil Mourya <45821949+NikhilMourya@users.noreply.github.com>
2021-08-20 06:03:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -43,6 +43,36 @@ function dispData(){
 }
 dispData();
 
+//To sort table data after clicking on header
+$(function () {  
+  $('table')  
+    .on('click', 'th', function () {  
+      var index = $(this).index(),  
+          rows = [],  
+          thClass = $(this).hasClass('asc') ? 'desc' : 'asc';  
+      $('#ceo th').removeClass('asc desc');  
+      $(this).addClass(thClass);  
+      $('#ceo tbody tr').each(function (index, row) {  
+        rows.push($(row).detach());  
+      });  
+      rows.sort(function (a, b) {  
+        var aValue = $(a).find('td').eq(index).text(),  
+            bValue = $(b).find('td').eq(index).text();  
+        return aValue > bValue  
+             ? 1  
+             : aValue < bValue  
+             ? -1  
+             : 0;  
+      });  
+      if ($(this).hasClass('desc')) {  
+        rows.reverse();  
+      }  
+      $.each(rows, function (index, row) {  
+        $('#ceo tbody').append(row);  
+      });  
+    });  
+});   
+
 //All input must have values to prevent array size issue in table
 /*(function () {
     $('form>input').keyup(function () {
